206e037a23bb6949be6e224450d7bc3f1e6c7422
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-04-25 22:18:36 +0200

    added simple command handler

---
+++ FirstMediatr/Functions/Command/UpdateBookCommand.cs
@@ -0,0 +1,17 @@
+﻿using FirstMediatr.Model;
+using MediatR;
+
+namespace FirstMediatr.Functions.Command
+{
+    public class UpdateBookCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public int Rate { get; set; }
+    }
+}
---
+++ FirstMediatr/Functions/Command/UpdateBookCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using FirstMediatr.DummyData;
+using FirstMediatr.Model;
+using MediatR;
+
+namespace FirstMediatr.Functions.Command
+{
+    public class UpdateBookCommandHandler : IRequestHandler<UpdateBookCommand>
+    {
+        public Task Handle(UpdateBookCommand request, CancellationToken cancellationToken)
+        {
+            var book = new Book
+            {
+                Author = request.Author,
+                Category = request.Category,
+                CategoryId = request.CategoryId,
+                Date = request.Date,
+                Description = request.Description,
+                Id = request.Id,
+                Rate = request.Rate,
+                Title = request.Title,
+            };
+            int index = StaticData.Books.FindIndex(x => x.Id== request.Id);
+            if(index == -1)
+            {
+                StaticData.Books[index] = book;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
